7727708d778badee04a390c04f89803a3c121053
Jim Bourne <jbourne@streamsystems.ca>
2023-04-28 09:39:31 -0600

    Enhancements
    Corrected daemonset so that the  pods are picked up (annotation)
    
    Added more and varied log entries

--- log-script.sh
+++ log-script.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 while true ; do
+# regular log
   TS=`date +"%Y-%m-%d %H:%M:%S"`
   D=`date +%s`
   echo "[$TS] $0 Logging at $D"
-  sleep 10
+# Random sleep time
+  let j="10* $RANDOM / 32767"
+  sleep $j
+# Error log
+  TS=`date +"%Y-%m-%d %H:%M:%S"`
+  D=`date +%s`
+  echo "[$TS] $0 ERROR: Logging at $D"
+# warning log
+  TS=`date +"%Y-%m-%d %H:%M:%S"`
+  D=`date +%s`
+  echo "[$TS] $0 WARN: Logging at $D"
+  
 done
 
